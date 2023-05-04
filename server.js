e8bf801e97b868ae3899f4da37c6320fe622754c
Konstanty Wrona <koswro@gmail.com>
2023-05-04 07:24:02 +0200

    Implement socket to server

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
+
+const tasks = [];
+
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running...');
 });
 
 app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('Client connected with ID:', socket.id);
+  socket.emit('updateData', tasks);
+  socket.on('addTask', (task) => {
+    console.log('Task added');
+    tasks.push(task);
+    socket.broadcast.emit('addTask', task);
+  });
+  socket.on('removeTask', (task) => {
+    tasks.splice(tasks.indexOf(task), 1);
+    socket.broadcast.emit('removeTask', task);
+  });
 });
